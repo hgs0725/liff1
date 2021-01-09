b6797d61caf49df219b60b4fe4c443b55ee4fdae
hgs0725 <naro4493@gmail.com>
2021-01-10 01:10:47 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -29,28 +29,6 @@ window.onload = function (e) {
     });
 };
 
-// Get profile and display
-function getProfile() {
-    // https://developers.line.me/en/reference/liff/#liffgetprofile()
-    liff.getProfile().then(function (profile) {
-        document.getElementById('useridprofilefield').textContent = profile.userId;
-        document.getElementById('displaynamefield').textContent = profile.displayName;
-
-        var profilePictureDiv = document.getElementById('profilepicturediv');
-        if (profilePictureDiv.firstElementChild) {
-            profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-        }
-        var img = document.createElement('img');
-        img.src = profile.pictureUrl;
-        img.alt = "Profile Picture";
-        img.width = 200;
-        profilePictureDiv.appendChild(img);
-
-        document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-    }).catch(function (error) {
-        window.alert("Error getting profile: " + error);
-    });
-}
 
 function initializeApp(data) {
     document.getElementById('languagefield').textContent = data.language;
